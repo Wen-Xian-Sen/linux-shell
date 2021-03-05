48358274f542eb3d5ff9ab9e801f3823d0801cad
WenDongBaoPC <18277375641@163.com>
2021-03-05 11:37:13 +0800

    repir bug

--- autoDel.sh
+++ autoDel.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-#指定需要删除的文件的路径
-historyDir=/home/firefly/plc-gateway/bin/ 
+historyDir=/home/firefly/shell/test/ #指定需要删除的文件的路径
 
 today=$(date +%Y-%m-%d) #当前日期
 
-echo "-------shell file auto start-------"
-echo "----shell file :/etc/profile.d/---- "
+echo "------------------------------------"
+echo "开机自动脚本"
+echo "在/etc/profile.d/目录下"
 echo "---------today is $today-----------"
 
-#保存7 days之前的时间值
-tt=`date -d "7 days ago" +%s`  
+tt=`date -d "6 second ago" +%s`  #保存7 days之前的时间值
 
 echo "next is to delete release before $tt"
 
@@ -18,25 +17,26 @@ for file in ${historyDir}*
 do
         name=`basename $file`
 
-        #打印文件名
-		#echo "file name : $name"  
-		#保存文件修改时间值
-        file_date=`date -r ${historyDir}${name} +%s` 
-		#打印文件日期
-		#echo "file date : $file_date" 
+        #echo "file name : $name"  #打印文件名
+
+        file_date=`date -r ${historyDir}${name} +%s` #保存文件时间值
+
+	#echo "file date : $file_date" #打印文件日期
 
         if [ $file_date -le $tt ] #比较文件时间值和7 days之前时间值
             then
-			    #跳过不需要删除的文件
-                if [[ $name == "add-eth-static" || $name == "autoDel.sh" || $name == "add-wifi-connection" || $name == "deviceConfig.txt" || $name == "deviceInfo.txt" || $name == "keystore.p12" || $name == "plc-gateway" || $name == "plc-gateway.bat" || $name == "plc-gateway.log" ]]
-                    then
+		str=${name:0:16}
+		echo $str
+                if [ "$str" = "plc-gateway.log." ]    
+	    	then
+                        echo " delete ${historyDir}$name-------"
+                        rm -rf ${historyDir}${name}  #删除命令
+                else
                         echo --pass this file : $name
                         continue;
-                else
-                        echo " delete $name-------"
-                        rm -rf ${historyDir}${name} 
-                fi
+		fi
         fi
 done
+
 
 
